afb18cc92dc585853e06f809d54f0db942bb6f55
Siddharth Sahani <sidhucroedde@gmail.com>
2024-07-26 18:16:39 +0530

    feat: more tiles for world gen

--- src/worldgen.cpp
+++ src/worldgen.cpp
@@ -6,11 +6,28 @@ std::vector<Tile> WorldGen::createWorld(int boundX, int boundY)
     // creating a vec of fixed size
     std::vector<Tile> tiles(boundX * boundY);
 
+    Image noiseImage = GenImagePerlinNoise(boundX, boundY, 0, 0, 2);
+    Color *noiseData = (Color *)noiseImage.data;
+
     for (int y = 0; y < boundY; y++)
     {
         for (int x = 0; x < boundX; x++)
         {
-            tiles[x + y * boundY] = Tile::Grass;
+            const float val = noiseData[x + y * boundX].r / 255.0f;
+            Tile tile;
+            if (val < 0.3)
+            {
+                tile = Tile::Dirt;
+            }
+            else if (val < 0.7)
+            {
+                tile = Tile::Weed;
+            }
+            else
+            {
+                tile = Tile::Grass;
+            }
+            tiles[x + y * boundX] = tile;
         }
     }
 
@@ -22,10 +39,15 @@ Texture WorldGen::createWorldTexture(int boundX, int boundY)
     // how crisp the texture is (16 is a good number for now)
     const float crispFactor = 16;
 
-    Texture tiles[] = {
-        LoadTexture("assets/grass.png"),
+    Texture assets[] = {
+        LoadTexture("assets/dirt_1.png"),
+        LoadTexture("assets/dirt_2.png"),
+        LoadTexture("assets/weeded_grass_1.png"),
+        LoadTexture("assets/weeded_grass_2.png"),
+        LoadTexture("assets/grass_1.png"),
+        LoadTexture("assets/grass_2.png"),
     };
-    const int numTiles = sizeof(tiles) / sizeof(Texture);
+    const int numAssets = sizeof(assets) / sizeof(Texture);
 
     const std::vector<Tile> worldData = createWorld(boundX, boundY);
     // the world will be drawn into this
@@ -37,19 +59,21 @@ Texture WorldGen::createWorldTexture(int boundX, int boundY)
     {
         for (int x = 0; x < boundX; x++)
         {
-            const Texture tex = tiles[(int)worldData[x + y * boundY]];
+            const Tile tile = worldData[x + y * boundX];
+            const int texIndex = GetRandomValue((int)tile * 2, (int)tile * 2 + 1);
+            const Texture tex = assets[texIndex];
             const Rectangle srcRect = {0, 0, (float)tex.width, (float)tex.height};
             const Rectangle destRect = {x * crispFactor, y * crispFactor, crispFactor, crispFactor};
-            const int rotation = GetRandomValue(0, 3);
-            
+            // const int rotation = GetRandomValue(0, 3);
+            const int rotation = 0;
+
             // the X origin changes when the angle is 180 or 270 degrees
             const float originX = (rotation == 2 || rotation == 3) ? 1 : 0;
             // the Y origin changes when the angle is 90 or 180 degrees
             const float originY = (rotation == 1 || rotation == 2) ? 1 : 0;
             const Vector2 origin = {originX * crispFactor, originY * crispFactor};
 
-            const Color tintColors[] = {RED, BLUE, ORANGE, BROWN};
-            DrawTexturePro(tex, srcRect, destRect, origin, rotation * 90, tintColors[rotation]);
+            DrawTexturePro(tex, srcRect, destRect, origin, rotation * 90, WHITE);
         }
     }
 
@@ -59,9 +83,9 @@ Texture WorldGen::createWorldTexture(int boundX, int boundY)
     renderTex.texture.id = 0;
 
     // unloading stuff
-    for (int i = 0; i < numTiles; i++)
+    for (int i = 0; i < numAssets; i++)
     {
-        UnloadTexture(tiles[i]);
+        UnloadTexture(assets[i]);
     }
     UnloadRenderTexture(renderTex);
 
--- src/worldgen.h
+++ src/worldgen.h
@@ -4,16 +4,17 @@
 #include <raylib/raylib.h>
 #include <vector>
 
-
-enum class Tile {
-    Grass = 0,
+enum class Tile
+{
+    Dirt = 0,
+    Weed = 1,
+    Grass = 2,
 };
 
-
-class WorldGen {
+class WorldGen
+{
 
 public:
     static std::vector<Tile> createWorld(int boundX, int boundY);
     static Texture createWorldTexture(int boundX, int boundY);
-
 };
