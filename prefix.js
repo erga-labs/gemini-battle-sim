9c7b25402e85eb67e98f458146428cf6e69345a9
Siddharth Sahani <sidhucroedde@gmail.com>
2024-08-12 18:08:33 +0530

    refactor: removed unused js functions

--- prefix.js
+++ prefix.js
@@ -26,51 +26,6 @@ Module.onerror = (event) => {
 };
 
 
-Module.geminiResponse = null;
-
-Module.call_getGeminiResponse = (secretKey) => {
-    if (secretKey === "testSecretKey" && Module.geminiResponse !== null) {
-        console.error("BUG: Calling gemini api route even if one is ongoing");
-        return;
-    }
-
-    const postUrl = "/api/gemini";
-    const headers = {
-        "Authorization": "Bearer yoursecretkey",
-    };
-    const body = {
-        "gameState": {
-            "attackers": [
-                [[1, 2], [3], [100]],
-                [[4, 5], [2], [150]]
-            ],
-            "defenders": [
-                [[6, 7], [1], [200]],
-                [[8, 9], [3], [250]]
-            ],
-            "wall_health": [
-                [[10], [11]],
-                [[12], [13]]
-            ],
-            "last_moves": [
-                ["player", "ai"]
-            ],
-            "world_size": [20, 20]
-        }
-    };
-
-    fetch(postUrl, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(body),
-    })
-    .then(async (response) => {
-        const json = await response.json();
-        Module.geminiResponse = json;
-    });
-};
-
-
 Module.initialGameState = null;
 
 Module.call_getInitialGameState = () => {
--- src/js_functions.h
+++ src/js_functions.h
@@ -6,14 +6,6 @@
 using emscripten::EM_VAL;
 using emscripten::val;
 
-// calls the gemini api route
-void call_getGeminiResponse();
-
-// gets the gemini response
-// if returns null, then it means that the response hasnt been generated yet
-// call the function again until it returns something other than null
-EM_VAL getGeminiResponse();
-
 // calls the init route
 void call_getInitialGameState();
 
